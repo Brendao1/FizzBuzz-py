487047124820b470c33486a29352ec0551e942db
“Brenda” <odonnell.brenda@gmail.com>
2019-03-06 13:54:15 +0000

    changes to unittest file, 'unsupported operand type error'

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,10 +4,12 @@ from fizzbuzz import FizzBuzz
 class TestSum(unittest.TestCase):
 
     def test_play(self):
-        self.assertEqual(play([3]) == "Fizz", "Should return Fizz")
+        f = FizzBuzz()
+        self.assertEqual(f.play([3]) == "Fizz", "Should return Fizz")
 
     def test_play(self):
-        self.assertEqual(play([2]) == 2, "Should return 2")
+        f = FizzBuzz()
+        self.assertEqual(f.play([2]) == 2, "Should return 2")
 
 if __name__ == "__main__":
     unittest.main()
